2c7d9dce6d0672894288e793cf521dbfad3419cb
Luis Angel Iñiguez Hernández <luis@iotekholding.com>
2017-08-07 15:49:49 -0500

    Routers added

---
+++ app/components/Battle.js
@@ -0,0 +1,14 @@
+let React = require('react');
+
+class Battle extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Battle</h1>
+            </div>
+        )
+    }
+}
+
+module.exports = Battle;
