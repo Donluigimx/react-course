d4d09a911d179e9349599a2560ba507441d045b8
Luis Angel Iñiguez Hernández <luigi.lahi@gmail.com>
2017-08-07 01:25:09 -0500

    lifecycle finished

---
+++ app/utils/api.js
@@ -0,0 +1,13 @@
+let axios = require('axios');
+
+module.exports = {
+    fetchPopularRepos: function (language) {
+        let encodeURI = window.encodeURI('https://api.github.com/search/' +
+            'repositories?q=stars:>1+languages:'+ language +
+            '&sort=stars&order=desc&type=Repositories');
+
+        return axios.get(encodeURI).then(function (response) {
+            return response.data.items;
+        });
+    }
+}
